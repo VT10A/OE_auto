3f1c07474e47c458cb39d817b16e5811006431e7
VT10A <152657392+VT10A@users.noreply.github.com>
2024-02-21 20:59:24 +0000

    Update OEapp.py

--- OEapp.py
+++ OEapp.py
@@ -62,7 +62,7 @@ def main():
         st.write("This is where you can view and modify the brand list.")
         
         # Load and display the brand list 
-       brand_list_path = "Brandlist.csv"  
+        brand_list_path = "Brandlist.csv"  
         brand_list = pd.read_csv(brand_list_path)
         st.write("Brand List:")
         st.write(brand_list)
